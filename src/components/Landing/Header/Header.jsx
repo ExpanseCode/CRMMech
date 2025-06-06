cb9645202431b52baf5295a45d82777b3941956f
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2025-06-06 18:38:38 +0300

    change header bg

--- src/components/Landing/Header/Header.jsx
+++ src/components/Landing/Header/Header.jsx
@@ -46,100 +46,96 @@ export default function Header() {
   }, []);
 
   return (
-    <motion.nav
-      initial={{ y: -100 }}
-      animate={{ y: 0 }}
-      transition={{ duration: 0.3 }}
-      // className={`fixed top-0 w-full z-50 transition-all duration-300 py-4 ${
-      //   isScrolled
-      //     ? 'backdrop-blur-lg bg-white/95 shadow-lg border-b border-gray-200/50'
-      //     : 'bg-transparent'
-      // }`}
-    >
-      <nav className={css.wrapper}>
-        <img src={logo} alt="logo" className={css.logo} />
-
-        <ul className={css.list}>
-          <li className={css.item}>
-            <div
-              className={css.link}
-              onClick={() => scrollToSection('problems')}
-            >
-              Проблеми
-            </div>
-          </li>
-          <li className={css.item}>
-            <div
-              className={css.link}
-              onClick={() => scrollToSection('features')}
-            >
-              Можливості
-            </div>
-          </li>
-          <li className={css.item}>
-            <div
-              className={`${css.link} ${isActive && css.active}`}
-              onClick={() => setIsActive(!isActive)}
-            >
-              <div className={css.moduleWrapper} ref={wrapperPopupRef}>
-                Модулі
-                <IoIosArrowDown
-                  className={`${css.icon} ${isActive && css.rotated}`}
-                />
+    <div className={css.header}>
+      <motion.nav
+        initial={{ y: -100 }}
+        animate={{ y: 0 }}
+        transition={{ duration: 0.3 }}
+      >
+        <nav className={css.wrapper}>
+          <img src={logo} alt="logo" className={css.logo} />
+          <ul className={css.list}>
+            <li className={css.item}>
+              <div
+                className={css.link}
+                onClick={() => scrollToSection('problems')}
+              >
+                Проблеми
               </div>
-            </div>
-            <AnimatePresence>
-              {isActive && (
-                <motion.div
-                  initial={{ opacity: 0, y: 0 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  exit={{ opacity: 0, y: 0 }}
-                  transition={{ duration: 0.2 }}
-                >
-                  <Popup />
-                </motion.div>
-              )}
-            </AnimatePresence>
-          </li>
-          <li className={css.item}>
-            <div
-              className={css.link}
-              onClick={() => scrollToSection('pricing')}
+            </li>
+            <li className={css.item}>
+              <div
+                className={css.link}
+                onClick={() => scrollToSection('features')}
+              >
+                Можливості
+              </div>
+            </li>
+            <li className={css.item}>
+              <div
+                className={`${css.link} ${isActive && css.active}`}
+                onClick={() => setIsActive(!isActive)}
+              >
+                <div className={css.moduleWrapper} ref={wrapperPopupRef}>
+                  Модулі
+                  <IoIosArrowDown
+                    className={`${css.icon} ${isActive && css.rotated}`}
+                  />
+                </div>
+              </div>
+              <AnimatePresence>
+                {isActive && (
+                  <motion.div
+                    initial={{ opacity: 0, y: 0 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: 0 }}
+                    transition={{ duration: 0.2 }}
+                  >
+                    <Popup />
+                  </motion.div>
+                )}
+              </AnimatePresence>
+            </li>
+            <li className={css.item}>
+              <div
+                className={css.link}
+                onClick={() => scrollToSection('pricing')}
+              >
+                Тарифи
+              </div>
+            </li>
+            <li className={css.item}>
+              <div
+                className={css.link}
+                onClick={() => scrollToSection('feedback')}
+              >
+                Відгуки
+              </div>
+            </li>
+            <li className={css.item}>
+              <div className={css.link} onClick={() => scrollToSection('faq')}>
+                FAQ
+              </div>
+            </li>
+          </ul>
+          <div className={css.btnBox}>
+            <a
+              href="https://app.crmmech.com/"
+              target="blank"
+              className={css.enterLink}
             >
-              Тарифи
-            </div>
-          </li>
-          <li className={css.item}>
-            <div
-              className={css.link}
-              onClick={() => scrollToSection('feedback')}
+              Вхід в систему
+            </a>
+            <a
+              href="https://forms.gle/oShc79zDeNqAyQ5p8"
+              target="_blank"
+              className={css.freeTestLink}
             >
-              Відгуки
-            </div>
-          </li>
-          <li className={css.item}>
-            <div className={css.link} onClick={() => scrollToSection('faq')}>
-              FAQ
-            </div>
-          </li>
-        </ul>
-        <div className={css.btnBox}>
-          <a
-            href="https://app.crmmech.com/"
-            target="blank"
-            className={css.enterLink}
-          >
-            Вхід в систему
-          </a>
-          <a
-            href="https://forms.gle/oShc79zDeNqAyQ5p8"
-            target="_blank"
-            className={css.freeTestLink}
-          >
-            Замовити
-          </a>
-        </div>
-      </nav>
-    </motion.nav>
+              Замовити
+            </a>
+          </div>
+        </nav>
+      </motion.nav>
+    </div>
   );
 }
