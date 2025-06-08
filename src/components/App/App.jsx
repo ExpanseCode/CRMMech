da3fab16ee78e4f42da275e53a54bd138679ad46
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2025-06-08 16:54:47 +0300

    create routes and components for Module pages

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,8 @@ import './App.css';
 import Layout from '../Layout/Layout.jsx';
 import { Toaster } from 'react-hot-toast';
 import LandingPage from '../../pages/LandingPage/LandingPage.jsx';
+import AllModulesPage from '../../pages/AllModulesPage/AllModulesPage.jsx';
+import ModuleDetailsPage from '../../pages/ModuleDetailsPage/ModuleDetailsPage.jsx';
 
 function App() {
   return (
@@ -11,12 +13,12 @@ function App() {
         <Toaster />
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          {/* <Route path="/order" element={OrderPage} />
-        <Route path="/admin" element={AdminPage} />
-        <Route path="/api-docs" element={ApiDocsPage} />
-        <Route path="/modules" element={ModulesPage} />
-        <Route path="/modules/:id" element={ModuleDetailPage} />
-        <Route element={NotFound} /> */}
+          {/* <Route path="/order" element={OrderPage} /> */}
+          {/* <Route path="/admin" element={AdminPage} /> */}
+          {/* <Route path="/api-docs" element={ApiDocsPage} /> */}
+          <Route path="/modules" element={<AllModulesPage />} />
+          <Route path="/modules/:id" element={<ModuleDetailsPage />} />
+          <Route element={NotFound} />
         </Routes>
       </>
     </Layout>
