288e316b7548e5df97d630ff1563c19d8eaeb311
Valentine Ukho <petrova.hfhfhf@gmail.com>
2025-06-10 22:22:27 +0300

    start create Module details

---
+++ src/pages/ModuleDetailsPage/Card/Card.jsx
@@ -0,0 +1,58 @@
+import styles from './Card.module.css';
+import clsx from 'clsx';
+import React from 'react';
+
+
+export const Card = React.forwardRef(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={clsx(styles.card, className)}
+    {...props}
+  />
+));
+Card.displayName = "Card";
+
+export const CardHeader = React.forwardRef(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={clsx(styles.cardHeader, className)}
+    {...props}
+  />
+));
+CardHeader.displayName = "CardHeader";
+
+export const CardTitle = React.forwardRef(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={clsx(styles.cardTitle, className)}
+    {...props}
+  />
+));
+CardTitle.displayName = "CardTitle";
+
+export const CardDescription = React.forwardRef(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={clsx(styles.cardDescription, className)}
+    {...props}
+  />
+));
+CardDescription.displayName = "CardDescription";
+
+export const CardContent = React.forwardRef(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={clsx(styles.cardContent, className)}
+    {...props}
+  />
+));
+CardContent.displayName = "CardContent";
+
+export const CardFooter = React.forwardRef(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={clsx(styles.cardFooter, className)}
+    {...props}
+  />
+));
+CardFooter.displayName = "CardFooter";
