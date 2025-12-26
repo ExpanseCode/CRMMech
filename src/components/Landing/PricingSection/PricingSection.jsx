253e3468a5a2e5979fb109588e11e9da9ffd712c
JuliaPriadka <150530521+JuliaPriadka@users.noreply.github.com>
2025-12-26 17:26:00 +0200

    Updated tariffs section

--- src/components/Landing/PricingSection/PricingSection.jsx
+++ src/components/Landing/PricingSection/PricingSection.jsx
@@ -1,6 +1,18 @@
 import css from './PricingSection.module.css';
 import { motion } from 'framer-motion';
 import { Check, Star, ArrowRight } from 'lucide-react';
+import { useState } from 'react';
+import {
+  BsFillHouseFill,
+  BsShopWindow,
+  BsCheck2,
+  BsFillPersonFill,
+  BsPuzzleFill,
+  BsCameraVideo,
+  BsInfinity,
+  BsHandIndex,
+  BsCaretRightSquare,
+} from 'react-icons/bs';
 
 const plans = [
   {
@@ -58,6 +70,152 @@ const plans = [
   },
 ];
 
+const tariffs = [
+  {
+    name: 'Start',
+    textColor: 'textBlue',
+    headerIcon: <BsFillHouseFill />,
+    postsQuantity: '1 СТО',
+    price: 990,
+    features: [
+      { name: 'AI‑відео', status: false, quantity: 0, icon: <BsCameraVideo /> },
+      {
+        name: 'CRM / Облік / Склади',
+        status: true,
+        quantity: 2,
+        icon: <BsFillPersonFill />,
+      },
+      {
+        name: 'Інтеграція - ',
+        subName: 'постачальники',
+        status: true,
+        quantity: 3,
+        icon: <BsPuzzleFill />,
+      },
+      {
+        name: 'Додаток механіка',
+        status: true,
+        quantity: 2,
+        icon: <BsFillPersonFill />,
+      },
+      {
+        name: 'Додаток клієнта',
+        status: true,
+        quantity: -1,
+        icon: <BsFillPersonFill />,
+      },
+      { name: 'Аналітика', status: true },
+      { name: 'WEB розширення', status: false },
+    ],
+  },
+  // {
+  //   name: 'Full',
+  //   textColor: 'textBlue',
+  //   headerIcon: <BsShopWindow />,
+  //   postsQuantity: '1 - 3 пости',
+  //   price: 2990,
+  //   features: [
+  //     { name: 'AI‑відео', status: true, quantity: 4 , icon: <BsCameraVideo />},
+  //     { name: 'CRM / Облік / Склади', status: true, quantity: 4 ,icon:<BsFillPersonFill/> },
+  //     { name: 'Інтеграція - ', subName: 'постачальники',status: true, quantity: 10,icon:<BsPuzzleFill/> },
+  //     { name: 'Додаток механіка', status: true, quantity: 6,icon:<BsFillPersonFill/>  },
+  //     { name: 'Додаток клієнта', status: true, quantity: -1,icon:<BsFillPersonFill/>  },
+  //     { name: 'Аналітика', status: true },
+  //     { name: 'WEB розширення', status: true },
+  //   ],
+  // },
+  {
+    name: 'Profi',
+    textColor: 'textPurple',
+    headerIcon: <BsShopWindow />,
+    postsQuantity: '4 - 8 пости',
+    price: 4390,
+    features: [
+      { name: 'AI‑відео', status: true, quantity: 10, icon: <BsCameraVideo /> },
+      {
+        name: 'CRM / Облік / Склади',
+        status: true,
+        quantity: 8,
+        icon: <BsFillPersonFill />,
+      },
+      {
+        name: 'Інтеграція - ',
+        subName: 'постачальники',
+        status: true,
+        quantity: 20,
+        icon: <BsPuzzleFill />,
+      },
+      {
+        name: 'Додаток механіка',
+        status: true,
+        quantity: 20,
+        icon: <BsFillPersonFill />,
+      },
+      {
+        name: 'Додаток клієнта',
+        status: true,
+        quantity: -1,
+        icon: <BsFillPersonFill />,
+      },
+      { name: 'Аналітика', status: true },
+      { name: 'WEB розширення', status: true },
+    ],
+  },
+  {
+    name: 'Maximal',
+    textColor: 'textBlue',
+    headerIcon: <BsShopWindow />,
+    postsQuantity: 'от 9 постов',
+    price: 4390,
+    features: [
+      { name: 'AI‑відео', status: true, quantity: -1, icon: <BsCameraVideo /> },
+      {
+        name: 'CRM / Облік / Склади',
+        status: true,
+        quantity: -1,
+        icon: <BsFillPersonFill />,
+      },
+      {
+        name: 'Інтеграція - ',
+        subName: 'постачальники',
+        status: true,
+        quantity: -1,
+        icon: <BsPuzzleFill />,
+      },
+      {
+        name: 'Додаток механіка',
+        status: true,
+        quantity: -1,
+        icon: <BsFillPersonFill />,
+      },
+      {
+        name: 'Додаток клієнта',
+        status: true,
+        quantity: -1,
+        icon: <BsFillPersonFill />,
+      },
+      { name: 'Аналітика', status: true },
+      { name: 'WEB розширення', status: true },
+    ],
+  },
+  // {
+  //   name: 'Network',
+  //   textColor: 'textBlue',
+  //   headerIcon: <BsFillHouseFill />,
+  //   postsQuantity: 'від 3 СТО',
+  //   price: 5290,
+  //   features: [
+  //     { name: 'AI‑відео', status: true, quantity: -1 , icon: <BsCameraVideo />},
+  //     { name: 'CRM / Облік / Склади', status: true, quantity: -1,icon:<BsFillPersonFill/>  },
+  //     { name: 'Інтеграція - ', subName: 'постачальники',status: true, quantity: -1 ,icon:<BsPuzzleFill/>},
+  //     { name: 'Додаток механіка', status: true, quantity: -1 ,icon:<BsFillPersonFill/> },
+  //     { name: 'Додаток клієнта', status: true, quantity: -1,icon:<BsFillPersonFill/>  },
+  //     { name: 'Аналітика', status: true },
+  //     { name: 'WEB розширення', status: true },
+  //   ],
+  // },
+];
+
 const allFeatures = [
   '✅ CRM + планувальник',
   '✅ Відеоаналітика з камер',
@@ -76,22 +234,46 @@ export default function PricingSection() {
     }
   };
 
+  const toggleSwitch = () => {
+    setIsYear(!isYear);
+  };
+
+  const [isYear, setIsYear] = useState(false);
+
   return (
     <section id="pricing" className={css.section}>
       <div className={css.sectionWrapper}>
-        <motion.div
-          initial={{ opacity: 0, y: 30 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.6 }}
-          viewport={{ once: true }}
-          className={css.headerWrapper}
-        >
-          <h2 className={css.header}>💼 Тариф "Все включено"</h2>
-          <p className={css.headerText}>
-            Камери вже враховано у тарифі без доплат.
-          </p>
-        </motion.div>
-        <div className={css.cardsWrapper}>
+        <div className={css.tariffsWrapper}>
+          <motion.div
+            initial={{ opacity: 0, y: 30 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6 }}
+            viewport={{ once: true }}
+            className={css.headerWrapper}
+          >
+            {/* <h2 className={css.header}>💼 Тариф "Все включено"</h2> */}
+            <h2 className={css.header}>Тарифи</h2>
+            <div className={css.line}></div>
+            <p className={css.headerText}>
+              {/* Камери вже враховано у тарифі без доплат. */}
+              Все включено: камери, CRM, додатки, підтримка. Безкоштовний тест:
+              14 днів, без картки.
+            </p>
+            <div className={css.toggleWithTextWrapper}>
+              <p className={css.rightText}>Раз на місяць</p>
+              <label className={css.toggleSwitch}>
+                <input
+                  type="checkbox"
+                  checked={isYear}
+                  onChange={toggleSwitch}
+                />
+                <span className={css.slider} />
+              </label>
+              <p className={css.rightText}>за рік</p>
+              <p className={css.discount}>20% знижка</p>
+            </div>
+          </motion.div>
+          {/* <div className={css.cardsWrapper}>
           {plans.map((plan, index) => (
             <motion.div
               key={index}
@@ -133,8 +315,119 @@ export default function PricingSection() {
               </a>
             </motion.div>
           ))}
+        </div> */}
+          <div className={css.cardsWrapper}>
+            {tariffs.map((tariff, index) => (
+              <motion.div
+                key={index}
+                initial={{ opacity: 0, y: 30 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.6, delay: index * 0.1 }}
+                viewport={{ once: true }}
+                className={`${css.cardWrapper} ${
+                  tariff.name === 'Profi' && css.profi
+                }`}
+              >
+                <div className={`${css.stringWrapper} ${css.topString}`}>
+                  <h3 className={` ${css[tariff.textColor]} ${css.cardHeader}`}>
+                    {tariff.name}
+                  </h3>
+                  <div className={css.rightHeaderWrapper}>
+                    <p>{tariff.headerIcon}</p>
+                    <p>{tariff.postsQuantity}</p>
+                  </div>
+                </div>
+                <div className={`${css.stringWrapper} ${css.secondString}`}>
+                  {tariff?.name === 'Start' && !isYear && (
+                    <div
+                      className={`${css.paymentInfoWrapper} ${css.baseline}`}
+                    >
+                      <p className={css.price}>₴0</p>
+                      <p className={css.paymentInfo}>/ 14 днів, потім</p>
+                    </div>
+                  )}
+                  <div className={css.paymentInfoWrapper}>
+                    {isYear && (
+                      <p className={css.fullPrice}>₴{tariff?.price}</p>
+                    )}
+                    <p className={css.price}>
+                      ₴{!isYear ? tariff?.price : Number(tariff?.price) * 0.8}
+                    </p>
+                    {tariff?.name === 'Start' && isYear ? (
+                      <p className={css.paymentInfo}>/міс за локацію</p>
+                    ) : tariff?.name === 'Start' ? (
+                      <p className={css.paymentInfo}>/міс</p>
+                    ) : (
+                      <p className={css.paymentInfo}>/міс за локацію</p>
+                    )}
+                  </div>
+                </div>
+                <div className={css.listWrapper}>
+                  {tariff.features.map((feature, index) => (
+                    <div className={css.listString} key={index}>
+                      <BsCheck2
+                        className={`${css.checkIcon} ${
+                          feature.status === true
+                            ? css.trueCheckIcon
+                            : css.falseCheckIcon
+                        }`}
+                      />
+                      <p
+                        className={`${css.listText} ${
+                          feature.status === true
+                            ? css.trueListText
+                            : css.falseListText
+                        }`}
+                      >
+                        {feature.name}
+                        {feature?.subName && (
+                          <span className={css.suppliers}>
+                            {feature?.subName}
+                          </span>
+                        )}
+                      </p>
+                      {feature.status === true &&
+                        feature.name !== 'Аналітика' &&
+                        feature.name !== 'WEB розширення' && (
+                          <div className={css.quantityWrapper}>
+                            <p className={css.listIcon}>{feature.icon}</p>
+                            <p className={css.quantityText}>
+                              {feature.quantity !== -1 ? (
+                                feature.quantity
+                              ) : (
+                                <BsInfinity className={css.infinityIcon} />
+                              )}
+                            </p>
+                          </div>
+                        )}
+                    </div>
+                  ))}
+                </div>
+                <div className={`${css.stringWrapper} ${css.thirdString}`}>
+                  <p className={css.suppliers}>Apps — iOS/Android</p>
+                  <p className={css.paymentInfo}>скоро</p>
+                </div>
+                <a
+                  className={`${
+                    tariff.name === 'Profi' ? css.btnGradient : css.btnBlue
+                  } ${css.cardBtn}`}
+                  href="https://forms.gle/oShc79zDeNqAyQ5p8"
+                  target="_blank"
+                >
+                  {tariff.name === 'Start' ? (
+                    <BsCaretRightSquare />
+                  ) : (
+                    <BsHandIndex />
+                  )}
+                  {tariff.name === 'Start'
+                    ? 'Протестувати'
+                    : `Обрати ${tariff.name}`}
+                </a>
+              </motion.div>
+            ))}
+          </div>
         </div>
-        <motion.div
+        {/* <motion.div
           initial={{ opacity: 0, y: 30 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.6, delay: 0.4 }}
@@ -151,7 +444,7 @@ export default function PricingSection() {
               </p>
             ))}
           </div>
-        </motion.div>
+        </motion.div> */}
         <motion.div
           initial={{ opacity: 0, y: 30 }}
           whileInView={{ opacity: 1, y: 0 }}
